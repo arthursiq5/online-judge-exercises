661dea8bc9b36d958685fb49dac0d7cc19b9c747
arthursiq5 <arthur.siq5@gmail.com>
2021-07-30 16:17:54 -0300

    Adicionado exercício 1003 em C

---
+++ C/1003.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, ans;
+
+    scanf(" %i", &x);
+    scanf(" %i", &y);
+    
+    ans = x + y;
+
+    printf("SOMA = %i\n", ans);
+
+    return 0;
+}
