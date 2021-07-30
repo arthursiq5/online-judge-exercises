4260867ba995408424a5c4a802dacf9c5730f733
arthursiq5 <arthur.siq5@gmail.com>
2021-07-30 16:23:01 -0300

    Adicionado exercício 1006 em PHP

---
+++ PHP/1006.php
@@ -0,0 +1,9 @@
+<?php
+
+$num1 = floatval(readline());
+$num2 = floatval(readline());
+$num3 = floatval(readline());
+
+$media = (($num1 * 2) + ($num2 * 3) + ($num3 * 5)) / 10;
+
+printf("MEDIA = %.1f\n", $media);
