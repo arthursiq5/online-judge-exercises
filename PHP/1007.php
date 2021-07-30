cbfbb5ab85ca79a565d09659953e0936c793cf80
arthursiq5 <arthur.siq5@gmail.com>
2021-07-30 16:27:06 -0300

    Exercício 1007 em PHP

---
+++ PHP/1007.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = intval(readline());
+$b = intval(readline());
+$c = intval(readline());
+$d = intval(readline());
+
+$diferenca = $a * $b - $c * $d;
+
+printf("DIFERENCA = %d\n", $diferenca);
