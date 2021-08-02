c25284701e7a0acb7a83f26971e6362bd3acb1f7
arthursiq5 <arthur.siq5@gmail.com>
2021-08-02 19:35:15 -0300

    Exercício 1004 em C#

---
+++ C#/1004.cs
@@ -0,0 +1,12 @@
+using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int prod = num1 * num2;
+        Console.WriteLine("PROD = {0}", prod);
+    }
+}
