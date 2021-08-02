faa5a1e1c582f9eed9a67716333c922a001a577c
arthursiq5 <arthur.siq5@gmail.com>
2021-08-02 19:22:53 -0300

    Exercício 1003 em C#

---
+++ C#/1003.cs
@@ -0,0 +1,12 @@
+using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int soma = num1 + num2;
+        Console.WriteLine("SOMA = {0}", soma);
+    }
+}
