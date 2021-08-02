e265c5eeeb95898ba96de5a2c78adb193bc1adaa
arthursiq5 <arthur.siq5@gmail.com>
2021-08-02 16:27:26 -0300

    Exercício 1001 em C#

---
+++ C#/1001.cs
@@ -0,0 +1,11 @@
+using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("X = {0}", a + b);
+    }
+}
